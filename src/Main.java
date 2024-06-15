06b88d8234687b6266518b266d9da79a3eb10431
Shunior17 <juniorsitobustamante@gmail.com>
2024-06-15 07:24:11 -0600

    Serie 2.1. final

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    System.out.println("Bienvenido al menu de examen final\n" +
+            "Seleccione una opcion");
+    int op = 0;
+        Scanner sc= new Scanner(System.in);
+
+    switch (op){
+        case 1:{
+
+        }
+    }
+        }
+    }
